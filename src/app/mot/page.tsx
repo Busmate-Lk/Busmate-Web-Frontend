41833494ebf362e826822d0d53566f885294235d
ILakshitha <ilakshitha7921@gmail.com>
2025-06-20 11:15:25 +0530

    set folder structure

---
+++ src/app/mot/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/mot/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      mot
+
+    </div>
+    
+  );
+}
