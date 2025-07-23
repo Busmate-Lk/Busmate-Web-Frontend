b27e3cd0e57f62ff609873b6fabf3c8bb9a22561
Chamudi Upeka <129956853+chamudiupeka@users.noreply.github.com>
2025-07-23 21:28:54 +0530

    feat: Add ScheduleManagementRequest and ScheduleManagementResponse interfaces

---
+++ src/types/requestdto/schedule-management.ts
@@ -0,0 +1,18 @@
+import { time } from "console";
+
+export interface ScheduleManagementRequest {
+  name:  String ;
+  routeId: String ;
+  scheduleType:  String ;
+  effectiveStartDate:  Date ;
+  effectiveEndDate:  Date ;
+  status:  String ;
+  scheduleStops: [
+    {
+      stopId: String ;
+      stopOrder:number;
+      arrivalTime:String;
+      departureTime: String ;
+    }
+  ];
+}
